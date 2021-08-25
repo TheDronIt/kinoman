00a239dbe5582da5ac0e69de26d6776c6bf567f4
thedronit <andreykae@gmail.com>
2021-08-25 18:38:17 +1000

    2508211838

--- web/models.py
+++ web/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
-# Create your models here.
+class Film(models.Model):
+	class Meta:
+		verbose_name = 'Фильм'
+		verbose_name_plural = 'Фильм'
+	Age_list = [
+		("0+", "0+"),
+		("6+", "6+"),
+		("12+", "12+"),
+		("16+", "16+"),
+		("18+", "18+")
+	]
+	Category_list = [
+		("Боевик", "Боевик"),
+		("Вестерн", "Вестерн"),
+		("Гангстерский фильм", "Гангстерский фильм"),
+		("Детектив", "Детектив"),
+		("Драма", "Драма"),
+		("Исторический фильм", "Исторический фильм"),
+		("Комедия", "Комедия"),
+		("Мелодрама", "Мелодрама"),
+		("Музыкальный фильм", "Музыкальный фильм"),
+		("Нуар", "Нуар"),
+		("Политический фильм", "Политический фильм"),
+		("Приключенческий фильм", "Приключенческий фильм"),
+		("Сказка", "Сказка"),
+		("Трагедия", "Трагедия"),
+		("Трагикомедия", "Трагикомедия")
+	]
+	Name = models.CharField(verbose_name="Название", max_length=50)
+	Image = models.ImageField(verbose_name="Постер", upload_to='poster/')
+	Trailer = models.FileField(upload_to='videos/', null=True, blank=True, verbose_name="Трейлер")
+	Age = models.CharField(choices=Age_list, verbose_name="Возростное ограничение", max_length=50)
+	Category = models.CharField(choices=Category_list, verbose_name="Жанр", max_length=50)
+
+	def __str__(self):
+		return str(self.Name)
+
+
+
+class Seance(models.Model):
+	class Meta:
+		verbose_name = 'Сеанс'
+		verbose_name_plural = 'Сеанс'
+
+	Hall_list = [
+		("Зал1", "Зал1"),
+		("Зал2", "Зал2")
+	]
+	FilmName = models.ForeignKey(Film, on_delete=models.SET_NULL, null=True, blank=True)
+	SeanceDate = models.DateField(verbose_name="Дата сеанса")
+	SeanceTime = models.TimeField(verbose_name="Время сеанса")
+	Hall = models.CharField(verbose_name="Зал", choices=Hall_list, max_length=50)
+	Price = models.IntegerField(verbose_name="Цена")
+
+	def __str__(self):
+		return str(str(self.FilmName) + " " +str(self.SeanceDate) + " " + str(self.SeanceTime))
+			
--- web/views.py
+++ web/views.py
@@ -1,8 +1,65 @@
 from django.shortcuts import render
 from .models import *
+import datetime
+
 
 def index(requset):
-	data = {
+	film = Film.objects.all().order_by('id')
+	db_seance = Seance.objects.all().order_by('SeanceTime')
+	seance = []
+	for set_seance in db_seance:
+		seance.append(
+			dict(
+					FilmName = str(set_seance.FilmName),
+					SeanceDate = set_seance.SeanceDate.strftime('%d.%m.%Y'),
+					SeanceTime = set_seance.SeanceTime.strftime('%H:%M'),
+					Hall = set_seance.Hall,
+					Price = set_seance.Price
+				)
+			)
+	today = datetime.date.today()
+	tomorrow = today + datetime.timedelta(days=1)
+	day3 = today + datetime.timedelta(days=2)
+	day4 = today + datetime.timedelta(days=3)
+	day5 = today + datetime.timedelta(days=4)
+	day6 = today + datetime.timedelta(days=5)
+	day7 = today + datetime.timedelta(days=6)
+	day8 = today + datetime.timedelta(days=7)
 
+
+
+	calendar = []
+	for days in [today, tomorrow, day3, day4, day5, day6, day7, day8]:
+		if days == today:
+			day_of_week = "Сегодня"
+		elif days == tomorrow:
+			day_of_week = "Завтра"
+		else:
+			if days.weekday() == 0:
+				day_of_week = "Понедельник"
+			elif days.weekday() == 1:
+				day_of_week = "Вторник"
+			elif days.weekday() == 2:
+				day_of_week = "Среда"
+			elif days.weekday() == 3:
+				day_of_week = "Четверг"
+			elif days.weekday() == 4:
+				day_of_week = "Пятница"
+			elif days.weekday() == 5:
+				day_of_week = "Суббота"
+			elif days.weekday() == 6:
+				day_of_week = "Воскресенье" 
+		calendar.append(
+			dict(
+					Day = days.strftime('%d.%m'),
+					Day_of_week = day_of_week,
+					Link = "/date/"+days.strftime('%Y/%m/%d')
+				)
+			)
+	
+	data = {
+		'film': film,
+		'seance': seance,
+		'calendar': calendar
 	}
 	return render(requset, 'page/index.html', data)
